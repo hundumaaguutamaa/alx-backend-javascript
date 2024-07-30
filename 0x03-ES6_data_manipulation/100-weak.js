0b40b0c5ceacb31f1e5db82141779a409015c8fd
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-07-30 09:54:36 +0300

    Updated errors, indentation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,20 +1,17 @@
-// Export a const instance of WeakMap and name it weakMap. 
+// Export a const instance of WeakMap and name it weakMap.
 export const weakMap = new WeakMap();
-
 
 // Export new function queryAPI
 export function queryAPI(endpoint) {
-	\\ Check if endpoint exist in weakMap
-	if (!weakMap.has(endpoint)) {
-		\\ if not initialize count to 0
-		weakMap.set(endpoint, 0);
-	}
+    // Check if endpoint exists in weakMap
+    if (!weakMap.has(endpoint)) {
+        // if not initialize count to 0
+        weakMap.set(endpoint, 0);
+    }
 
-	\\ Increment endpoit 
-	const count = weakMap.get(endpoint) + 1;
-	if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
+    // Increment endpoint count
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+
+    if (count >= 5) throw new Error('Endpoint load is high');
 }
-}
-
-
-
