18a1c2eaca0a3f4548d293b67e7059b5e925a733
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-08-11 09:31:09 +0000

    New line ommited

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,14 @@
 // Car.js
-module.exports = Car;
+class Car {
+    constructor(brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
 
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-  }
-
-  cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
-  }
+    cloneCar() {
+        return new Car(this._brand, this._motor, this._color);
+    }
 }
+
+module.exports = Car;
