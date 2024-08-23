ee95fc1297ac2172be58c16189d285fd82e66483
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-23 03:28:29 +0300

    3-payment.test.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with the correct arguments', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    const consoleSpy = sinon.spy(console, 'log');
+    
+    sendPaymentRequestToApi(100, 20);
+    
+    expect(spy.calledOnceWith('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWith('The total is: 120')).to.be.true;
+    
+    spy.restore();
+    consoleSpy.restore();
+  });
+});
